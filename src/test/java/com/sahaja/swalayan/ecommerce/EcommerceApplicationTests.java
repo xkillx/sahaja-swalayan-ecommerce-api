7d06aea71329505dd106e833c5de6744b7a00b64
Azhar Khalid <azhar.tkjzone@gmail.com>
2025-07-15 09:30:49 +0700

    Initial commit: add all project files

---
+++ src/test/java/com/sahaja/swalayan/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahaja.swalayan.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
